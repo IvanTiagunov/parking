44ed62f365619fc6987c794800754be736170f5b
DonWithiT <tiagunov100@gmail.com>
2024-06-13 03:46:23 +0500

    +

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,6 +6,18 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing_extensions import Annotated
 
+from app.enums import Role
+
 
 def generate_uuid():
     return str(uuid4())
+
+def check_active(user):
+    if not user.is_active:
+        raise HTTPException(status_code=401,
+                             detail=f"Недостаточно прав. Пользователь деактивирован")
+def check_admin(user, error_msg):
+    if user.role_name != Role.admin:
+        raise HTTPException(status_code=401,
+                             detail=f"Недостаточно прав. {error_msg}")
+    check_active(user)
--- app/routers/user.py
+++ app/routers/user.py
@@ -7,6 +7,7 @@ from app.crud.auth import get_us
 from app.crud.user import create_user_crud, create_driver_crud, create_mechanic_crud, deactivate_user_crud, \
     get_list_users_crud, update_driver_crud, update_user_crud, check_user_not_exist, get_user_by_id_crud
 from app.db.database import get_db
+from app.dependencies import check_admin
 from app.enums import Role
 from app.schemas.user import DriverCreateData, MechanicCreateData, AdminCreateData, DriverUpdateData, UserUpdateFields, \
     DriverCreateResponseData, UserFromDB
@@ -16,18 +17,14 @@ router = APIRouter()
 
 @router.post("/admin/create", response_model=AdminCreateData, tags=['create'])
 async def create_admin(user: get_us, admin: AdminCreateData, session: Session = Depends(get_db)):
-    if user.role_name != Role.admin:
-        return HTTPException(status_code=401,
-                             detail="Недостаточно прав. Создавать администраторов может только администратор")
+    check_admin(user,"Создавать администраторов может только администратор")
     user = create_user_crud(session, admin)
     return user
 
 
 @router.post("/driver/create", tags=['create'])
 async def create_driver(user: get_us, driver_data: DriverCreateData, session: Session = Depends(get_db)):
-    if user.role_name != Role.admin:
-        return HTTPException(status_code=401,
-                             detail="Недостаточно прав. Создавать водителей может только администратор")
+    check_admin(user, "Создавать водителей может только администратор")
     user = create_user_crud(session, driver_data)
     driver = create_driver_crud(session, user, driver_data)
     return user, driver
@@ -35,9 +32,7 @@ async def create_driver(user: get_us, driver_data: DriverCreateData, session: Se
 
 @router.post("/mechanic/create", tags=['create'])
 async def create_mechanic(user: get_us, mechanic_data: MechanicCreateData, session: Session = Depends(get_db)):
-    if user.role_name != Role.admin:
-        return HTTPException(status_code=401,
-                             detail="Недостаточно прав. Создавать механиков может только администратор")
+    check_admin(user, "Создавать механиков может только администратор")
     user = create_user_crud(session, mechanic_data)
     mechanic = create_mechanic_crud(session, user)
     return mechanic
@@ -45,18 +40,14 @@ async def create_mechanic(user: get_us, mechanic_data: MechanicCreateData, sessi
 
 @router.post("/user/deactivate")
 async def deactivate_user(user: get_us, user_login_to_deactivate: str, session: Session = Depends(get_db)):
-    if user.role_name != Role.admin:
-        return HTTPException(status_code=401,
-                             detail="Недостаточно прав. Деактивировать пользователей может только администратор")
+    check_admin(user, "Деактивировать пользователей может только администратор")
     deactivate_user_crud(session, user_login_to_deactivate)
     return {"Успешно деактивирован пользователь с логином": {user_login_to_deactivate}}
 
 
 @router.get("/user/get_list", tags=['get_info'])
 async def get_user_list(user: get_us, session: Session = Depends(get_db)):
-    if user.role_name != Role.admin:
-        return HTTPException(status_code=401,
-                             detail="Недостаточно прав. Получить список пользователей может только администратор")
+    check_admin(user, "Получить список пользователей может только администратор")
     all_users = get_list_users_crud(session)
     return all_users
 
@@ -66,11 +57,8 @@ async def get_user_by_id(user: get_us, user_id, session: Session = Depends(get_d
     return user_from_db
 
 @router.post("/user/update", tags=['update'])
-async def update_user_data(user: get_us, user_fields: UserUpdateFields,
-                                        session: Session = Depends(get_db)):
-    if user.role_name != Role.admin:
-        return HTTPException(status_code=401,
-                             detail="Недостаточно прав. Обновить данные пользователя может только администратор")
+async def update_user_data(user: get_us, user_fields: UserUpdateFields, session: Session = Depends(get_db)):
+    check_admin(user, "Обновить данные пользователя может только администратор")
     user = update_user_crud(session, user_fields)
     return user
 
@@ -78,8 +66,6 @@ async def update_user_data(user: get_us, user_fields: UserUpdateFields,
 @router.post("/driver/update", tags=['update'])
 async def update_driver_car_access_type(user: get_us, driver_fields: DriverUpdateData,
                                         session: Session = Depends(get_db)):
-    if user.role_name != Role.admin:
-        return HTTPException(status_code=401,
-                             detail="Недостаточно прав. Обновить тип доступа к машинам у водителя может только администратор")
+    check_admin(user, "Обновить тип доступа к машинам у водителя может только администратор")
     driver = update_driver_crud(session, driver_fields)
     return driver
